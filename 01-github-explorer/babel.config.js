ed06ac844e3f37063146b5e63cf3bafda9f43ef4
Jean Monteiro <jeanmontt@hotmail.com>
2021-08-18 18:18:56 +0100

    Installing and configuring HTML Webpack Plugin and Webpack Dev Server

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react'
+    ['@babel/preset-react', { //configurando preset react para que não seja necessário importar o React em todo arquivo que utiliza jsx
+      runtime: 'automatic'
+    }]
   ]
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path'); //Webpack é rodado dentro do node, portanto as importações devem ser utilizando "require"
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //Importando o HTML Webpack Plugin
 
 module.exports = {
+  mode: 'development', //informando o modo de trabalho do webpack, podendo ser ele production ou development
   entry: path.resolve(__dirname, 'src', 'index.jsx'), //informa qual o arquivo inicial da aplicação. Utilizando o "path" para manter o padrão entre sistemas operacionais distintos.
   //com o "path" é utilizado a "," no lugar da "/" para informar o diretório, "__dirname" busca o diretório onde está este arquivo de instrução.
   output: { //informa qual arquivo será gerado pelo webpack
@@ -10,6 +12,14 @@ module.exports = {
   resolve: { //por padrão webpack lê arquivos .js, então é informado que tanto .js como .jsx devem ser lidos pela aplicação
     extensions: ['.js', '.jsx'],
   },
+  devServer: { //Configurando o Webpack Dev Server
+    contentBase: path.resolve(__dirname, 'public') //informa onde está o arquivo html da aplicação
+  },
+  plugins: [ //configurando o HTML Webpack Plugin
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html') //Qual arquivo de template que a ferramenta vai utilizar para gerar o HTML
+    })
+  ],
   module: { //onde fica as configurações de como a aplicação irá se comportar quando estiver importando cada um dos tipos de arquivos 
     rules: [ //Array de regras
       { //É criado um objeto para cada tipo de arquivo
