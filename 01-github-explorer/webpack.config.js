5d52a953f803a47b232cece480ba76230e00e04b
Jean Monteiro <jeanmontt@hotmail.com>
2021-08-24 11:50:19 +0100

    Styling the repository list

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -21,28 +21,28 @@ module.exports = {
     hot: true //Uma das configurações do React Refresh
   },
   plugins: [ //configurando o HTML Webpack Plugin
-    isDevelopment && new ReactRefreshWebpackPlugin(), //Verificando se o ambiente esta em desenvolvimento e se sim irá rodar o React Refresh
+    isDevelopment && new ReactRefreshWebpackPlugin(), //Verificando se o ambiente está em desenvolvimento e se sim irá rodar o React Refresh
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html') //Qual arquivo de template que a ferramenta vai utilizar para gerar o HTML
     })
-  ].filter(Boolean), //Devido a condicional adicionada para para que seja rodado o React Refresh em caso de ambiente de desenvolvimento, caso o ambiente seja de produção o retorno será "false", entretanto, "false" não é um plugin válido do Webpack, gerando erro. Para que isso não aconteça, é utilizado o ".filter(Boolean)" como hack, ou seja, ele filtra tudo aquilo que nao for um valor truthy e o remove
+  ].filter(Boolean), //Devido a condicional adicionada para para que seja rodado o React Refresh em caso de ambiente de desenvolvimento, caso o ambiente seja de produção o retorno será "false", entretanto, "false" não é um plugin válido do Webpack, gerando erro. Para que isso não aconteça, é utilizado o ".filter(Boolean)" como hack, ou seja, ele filtra tudo aquilo que não for um valor truthy e o remove
   module: { //onde fica as configurações de como a aplicação irá se comportar quando estiver importando cada um dos tipos de arquivos 
     rules: [ //Array de regras
       { //É criado um objeto para cada tipo de arquivo
         test: /\.jsx$/, //utilizado uma expressão regular para verificar se o arquivo é .jsx
-        exclude: /node_modules/, //exclui todos arquivos que estão dentro da pasta node_modules, pois por padrão os arquivos desta pasta já são arquivos prontos para leitura do browser e não deve ser modificado. Cada biblioteca deve se preocupar com seu próprio processo de build do aquivo para que o browser entenda.
+        exclude: /node_modules/, //exclui todos arquivos que estão dentro da pasta node_modules, pois por padrão os arquivos desta pasta já são arquivos prontos para leitura do browser e não deve ser modificado. Cada biblioteca deve se preocupar com seu próprio processo de build do arquivo para que o browser entenda.
         use: {
-          loader: 'babel-loader', //utiliza o Babel Loader para fazer a converção do arquivo.
+          loader: 'babel-loader', //utiliza o Babel Loader para fazer a conversão do arquivo.
           options: {
             plugins: [
               isDevelopment && require.resolve('react-refresh/babel') //Configuração do React Refresh
-            ].filter(Boolean) //Devido a condicional adicionada para para que seja rodado o React Refresh em caso de ambiente de desenvolvimento, caso o ambiente seja de produção o retorno será "false", entretanto, "false" não é um plugin válido do Webpack, gerando erro. Para que isso não aconteça, é utilizado o ".filter(Boolean)" como hack, ou seja, ele filtra tudo aquilo que nao for um valor truthy e o remove
+            ].filter(Boolean) //Devido a condicional adicionada para para que seja rodado o React Refresh em caso de ambiente de desenvolvimento, caso o ambiente seja de produção o retorno será "false", entretanto, "false" não é um plugin válido do Webpack, gerando erro. Para que isso não aconteça, é utilizado o ".filter(Boolean)" como hack, ou seja, ele filtra tudo aquilo que não for um valor truthy e o remove
           }
         }
       },
       {
         test: /\.scss$/, //utilizado uma expressão regular para verificar se o arquivo é .css
-        exclude: /node_modules/, //exclui todos arquivos que estão dentro da pasta node_modules, pois por padrão os arquivos desta pasta já são arquivos prontos para leitura do browser e não deve ser modificado. Cada biblioteca deve se preocupar com seu próprio processo de build do aquivo para que o browser entenda.
+        exclude: /node_modules/, //exclui todos arquivos que estão dentro da pasta node_modules, pois por padrão os arquivos desta pasta já são arquivos prontos para leitura do browser e não deve ser modificado. Cada biblioteca deve se preocupar com seu próprio processo de build do arquivo para que o browser entenda.
         use: ['style-loader', 'css-loader', 'sass-loader'] //utiliza o Style Loader, CSS Loader e SASS Loader para fazer a conversão do arquivo .scss
       }
     ],
