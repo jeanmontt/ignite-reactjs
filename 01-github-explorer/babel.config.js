32277c5a2c2fc5a039ce5fd9c7932e13503bfaea
Jean Monteiro <jeanmontt@hotmail.com>
2021-08-26 17:36:21 +0100

    Installing and configuring TypeScript

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     ['@babel/preset-react', { //configurando preset react para que não seja necessário importar o React em todo arquivo que utiliza jsx
       runtime: 'automatic'
     }]
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -7,14 +7,14 @@ const isDevelopment = process.env.NODE_ENV !== 'production' //Verificando se a v
 module.exports = {
   mode: isDevelopment ? 'development' : 'production', //informando o modo de trabalho do webpack, podendo ser ele development ou production
   devtool: isDevelopment ? 'eval-source-map' : 'source-map', //Configurando o Source Map, existem alguns modos de configurações, sendo o eval-source-map utilizada para desenvolvimento e source-map para produção
-  entry: path.resolve(__dirname, 'src', 'index.jsx'), //informa qual o arquivo inicial da aplicação. Utilizando o "path" para manter o padrão entre sistemas operacionais distintos.
+  entry: path.resolve(__dirname, 'src', 'index.tsx'), //informa qual o arquivo inicial da aplicação. Utilizando o "path" para manter o padrão entre sistemas operacionais distintos.
   //com o "path" é utilizado a "," no lugar da "/" para informar o diretório, "__dirname" busca o diretório onde está este arquivo de instrução.
   output: { //informa qual arquivo será gerado pelo webpack
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  resolve: { //por padrão webpack lê arquivos .js, então é informado que tanto .js como .jsx devem ser lidos pela aplicação
-    extensions: ['.js', '.jsx'],
+  resolve: { //por padrão webpack lê arquivos .js, então é informado podem ser lidos pela aplicação os .js, .jsx, .ts e .tsx
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: { //Configurando o Webpack Dev Server
     contentBase: path.resolve(__dirname, 'public'), //informa onde está o arquivo html da aplicação
@@ -29,7 +29,7 @@ module.exports = {
   module: { //onde fica as configurações de como a aplicação irá se comportar quando estiver importando cada um dos tipos de arquivos 
     rules: [ //Array de regras
       { //É criado um objeto para cada tipo de arquivo
-        test: /\.jsx$/, //utilizado uma expressão regular para verificar se o arquivo é .jsx
+        test: /\.(j|t)sx$/, //utilizado uma expressão regular para verificar se o arquivo é .jsx ou .tsx
         exclude: /node_modules/, //exclui todos arquivos que estão dentro da pasta node_modules, pois por padrão os arquivos desta pasta já são arquivos prontos para leitura do browser e não deve ser modificado. Cada biblioteca deve se preocupar com seu próprio processo de build do arquivo para que o browser entenda.
         use: {
           loader: 'babel-loader', //utiliza o Babel Loader para fazer a conversão do arquivo.
